b05545825e9fa4ef92f29b212dcd2fe24d38ab9b
Alex <tekillaprest@gmail.com>
2020-06-22 11:51:53 +0700

    Admin(js and css)

--- ForAfterwind/wwwroot/js/Admin.js
+++ ForAfterwind/wwwroot/js/Admin.js
@@ -8,5 +8,26 @@ $(document).ready(function () {
     $("#songs").submit();
     $("#videos").submit();
 
-
 });
+
+function removeSkillsFormEvents() {
+    $("#skills").submit();
+    $("#addskill").trigger("reset");
+}
+
+function removeLinksFormEvents() {
+    $("#links").submit();
+    $("#addlink").trigger("reset");
+}
+function removePhotosFormEvents() {
+    $("#photos").submit();
+    $("#addphotos").trigger("reset");
+}
+function removeSongsFormEvents() {
+    $("#songs").submit();
+    $("#addsongs").trigger("reset");
+}
+function removeVideosFormEvents() {
+    $("#videos").submit();
+    $("#addvideo").trigger("reset");
+}
